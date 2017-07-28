15ec2758724550c831ac0534485474fa43911389
Studog <645546068@qq.com>
2017-07-28 16:01:27 +0800

    Update read_dong_data.py

--- read_dong_data.py
+++ read_dong_data.py
@@ -4,87 +4,69 @@ Created on Wed Dec 14 10:53:35 2016
 
 @author: Laresh
 """
-from collections import Counter
 from math import sqrt
 from decimal import Decimal
 from itertools import combinations
 import csv
 import pandas as pd
-
-#excel_read = pd.ExcelFile(r'C:\Users\Administrator\Desktop\exercise2_ans_history.xls')
-
+ 
 def get_index(table):
-    r = len(table)
-    c = len(table.columns)
-    answer = []
-    record = []
-    for i in range(r):
+    total = len(table)
+    columns = len(table.columns)
+    # k:题号,v:次数
+    all_items = {}
+    # 所有题号
+    answers = []
+    for i in range(total):
         temp = []
-        for j in range(4,c+1,2):
-            t=table.iloc[i,j]
-            if t < 0:
-                s=table.iloc[i,j-1]
-                temp.append(s)
+        for j in range(4, columns+1, 2):
+            score = table.iloc[i, j]
+            if score < 0:
+                test_num = table.iloc[i, j-1]
+                temp.append(test_num)
+                all_items[test_num] = all_items.setdefault(test_num, 0) + 1
         if temp:
-            answer.extend(temp)
-            record.append(temp)
-    return answer, record
+            answers.append(temp)
+    return answers, all_items, total
+ 
+def freq_2(answers, all_items):
+    all_pairs = []
+    all_pairsdict = {}
+    for item in answers:
+        temp = list(combinations(item, 2))
+        all_pairs.extend(temp)
+    for pair in all_pairs:
+        all_pairsdict[pair] = all_pairsdict.setdefault(pair, 0) + 1
+    return all_pairsdict
 
-def freq_2(answer, record):
-    count = 0
-    answer_pair = []
-    answer_con = []
-    record_len = 0
-    for item in record:
-        temp = list(combinations(item,2))
-        answer_pair.extend(temp)
-        record_len += 1            
-    answer_count = Counter(answer).most_common()
-    answer_pair_count = Counter(answer_pair).most_common()
-    for kv in answer_pair_count:
-        temp = []
-        for i in answer_count:
-            if i[0] in kv[0]:
-                temp.append(i)
-                count += 1
-                if count == 2:
-                    temp.append(kv)
-                    count = 0
-                    answer_con.append(temp)
-    return answer_con, record_len
 '''
 write the correlative record into csv file, calculate the rho. 
 '''
-def csv_write(answer_con, record_len):
-    csvfile = file(r'f:\another_test\real_data.csv','wb')
+
+def csv_write(total, all_pairsdict, all_items):
+    csvfile = file(r'C:\Users\Administrator\Desktop\data.csv', 'wb')
     writer = csv.writer(csvfile)
-    writer.writerow(['Num','Occu','Num','Occu','Corr_oc','Sup','Rho'])
-    for con in answer_con:
-        na = con[0][0]
-        nb = con[1][0]
-        ta = con[0][1]
-        tb = con[1][1]
-        tab = con[2][1]
-        sup = Decimal(1.0*tab/record_len).quantize(Decimal('0.00'))
-        up_value = tab*record_len - ta*tb
-        bottom_value = sqrt(ta*(record_len - ta)*tb*(record_len - tb))
-        if bottom_value != 0 and up_value >=0 and tab >= 5:
-        # if bottom_value != 0 and up_value >= 0:
-            rou = Decimal(up_value/bottom_value).quantize(Decimal('0.00'))
-            data = [na,ta,nb,tb,tab,sup,rou]
-            writer.writerow(data)
+    writer.writerow(['Num', 'Occu', 'Num', 'Occu', 'Corr_oc', 'Rho'])
+    for k, v in all_pairsdict.iteritems():
+        if k[0] in all_items.keys() and k[1] in all_items.keys():
+            up_value = v*total-all_items[k[0]]*all_items[k[1]]
+            bottom_value = sqrt(all_items[k[0]]*(total-all_items[k[0]])* \
+            all_items[k[1]]*(total-all_items[k[1]]))
+            if bottom_value != 0 and up_value >= 0:
+                rou = Decimal(up_value/bottom_value).quantize(Decimal('0.00'))
+                data = [k[0], all_items[k[0]], k[1], all_items[k[1]], v, rou]
+                writer.writerow(data)
     csvfile.close()
-
+ 
 def main():
-    path = raw_input('please input the file path:\n')
-    table = pd.read_excel(path,sheetname=0,header=0,encoding='utf-8')
-    answer, record = get_index(table)
-    answer_con, record_len = freq_2(answer, record)
-    csv_write(answer_con, record_len)
+    path = r'C:\Users\Administrator\Desktop\a.xls'
+    table = pd.read_excel(path, sheetname=0, header=0, encoding='utf-8')
+    answers, all_items, total = get_index(table)
+    all_pairsdict = freq_2(answers, all_items)
+    csv_write(total, all_pairsdict, all_items)
     print 'file successfully written!'
-
+ 
 if __name__ == '__main__':
     main()
-
             
    
